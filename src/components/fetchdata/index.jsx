2178a0844b0b9643f1ca4159954d67b2a4994083
Mohamed Afzar <mohamed@divum.in>
2023-11-29 16:55:23 +0530

    login api success

--- src/components/fetchdata/index.jsx
+++ src/components/fetchdata/index.jsx
@@ -1,43 +1,43 @@
- const fetchdata = async () => {
-      try {
-        const response = await fetch('http://localhost:5000/login');
-        const data = await response.json();
-        return data;
-      } catch (error) {
-        console.log('Error fetching data:', error);
-        return [];
-      }
-    }
+//  const fetchdata = async () => {
+//       try {
+//         const response = await fetch('http://localhost:5000/login');
+//         const data = await response.json();
+//         return data;
+//       } catch (error) {
+//         console.log('Error fetching data:', error);
+//         return [];
+//       }
+//     }
     
-    const fetchHomeDetails = async () => {
-      const response = await fetch('http://localhost:5000/login');
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-    };
+//     const fetchHomeDetails = async () => {
+//       const response = await fetch('http://localhost:5000/login');
+//       if (!response.ok) {
+//         throw new Error('Network response was not ok');
+//       }
+//       return response.json();
+//     };
 
-    const HomeScreen = () => {
-        const { data, error, isLoading } = useData();
-        // const { data, error, isLoading } = useQuery('products', fetchProducts);  
+//     const HomeScreen = () => {
+//         const { data, error, isLoading } = useData();
+//         // const { data, error, isLoading } = useQuery('products', fetchProducts);  
       
-        if (isLoading) {
-          return <div>Loading...</div>;
-        }
+//         if (isLoading) {
+//           return <div>Loading...</div>;
+//         }
       
-        if (error) {
-          return <div>Error: {error.message}</div>;
-        }
+//         if (error) {
+//           return <div>Error: {error.message}</div>;
+//         }
       
-        return (
-          <div>
-            {/* {data.map(() => (
-              <div key={product.id}>
-                <p>{product.title}</p>
-                <p>${product.price}</p>
-              </div>
-            ))} */}
-          </div>
-        );
-      };
+//         return (
+//           <div>
+//             {/* {data.map(() => (
+//               <div key={product.id}>
+//                 <p>{product.title}</p>
+//                 <p>${product.price}</p>
+//               </div>
+//             ))} */}
+//           </div>
+//         );
+//       };
       
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,72 +1,91 @@
-import React, { useState } from 'react'
-import Heading from '../../components/heading'
-import Textarea from '../../components/textbox'
-import Button from '../../components/button'
-import './home.css'
-import Account from '../../components/register'
-import axios from 'axios'
-import Login from '../../components/login'
-import { Images } from '../../assets/Images'
+import React, { useState } from "react";
+import Heading from "../../components/heading";
+import Textarea from "../../components/textbox";
+import Button from "../../components/button";
+import "./home.css";
+import Account from "../../components/register";
+import axios from "axios";
+import Login from "../../components/login";
+import { Images } from "../../assets/Images";
 // import {observer} from 'mobx-react-lite';
 // import { Observer } from 'mobx-react-lite'
 // import ProjectStore from '../../Apistore'
 
 const Home = () => {
+  const [emailOrUsername, setEmailOrUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [emailOrUsername, setEmailOrUsername] = useState('')
-    const [password, setPassword] = useState('')
+  const handleLogin = () => {
+    // console.log(`logging username and password: ${emailOrUsername} ${password}`);
+    axios
+      .post("http://localhost:5000/login", {
+        email: emailOrUsername,
+        password: password,
+      })
+      .then(
+        (response) => {
+          localStorage.setItem(
+            "access-token",
+            response?.data?.result?.accessToken
+          );
+          localStorage.setItem(
+            "refresh-token",
+            response?.data?.result?.refreshToken
+          );
+          console.log(response, "response");
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  };
 
-    const handleLogin = () => {
-        // console.log(`logging username and password: ${emailOrUsername} ${password}`);
-        axios.post('/ http://localhost:5000/login', {
-            'email': emailOrUsername,
-            'password': password
-          })
-          .then((response) => {
-            console.log(response,'response');
-          }, (error) => {
-            console.log(error); 
-          })
-    }
+  const handleForgotPassword = () => {
+    console.log(`sending password reset email to ${emailOrUsername}`);
+  };
 
-    const handleForgotPassword = () => {
-        console.log(`sending password reset email to ${emailOrUsername}`)
-    }
+  const handleChange = (e) => {
+    setEmailOrUsername(e.target.value);
+  };
+  const handlePassChange = (e) => {
+    setPassword(e.target.value);
+  };
 
-    const handleChange = (e) => {
-        setEmailOrUsername(e.target.value);
-    };
-    const handlePassChange = (e) => {
-        setPassword(e.target.value);
-    };
-
-
-
-
-    return (
-        <div className='homeinfo'>
-            <div className='left-part' >
-                <img src={Images.wallpaper} alt="" className='left_image' />
-                <h1 className='Lorem-ipsum'>LOREM IPSUM</h1>
-            </div>
-            <div className='right-part'>
-                <Heading title={"Welcome to Lorem Ipsum!"} />
-                <div className='details'>
-                    <Textarea label={"Email/ User Name"} placeholder={"e.g. John Joe"} typevalue={'email'} value={emailOrUsername} onChange={handleChange} />
-                    <Textarea label={"password"} placeholder={"Enter Password"} typevalue={'password'} value={password} onChange={handlePassChange} />
-                </div>
-                <div className='forgot-pass' onClick={handleForgotPassword}>
-                    <p>Forgot Password?</p>
-                </div>
-                <Button name={"Login"} className={"log"} onChange={handleLogin} />
-                <div className='account'>
-                    <Account name={"Don't have an account?"} />
-                    <Login value={"Register now"} />
-                </div>
-
-            </div>
+  return (
+    <div className="homeinfo">
+      <div className="left-part">
+        <img src={Images.wallpaper} alt="" className="left_image" />
+        <h1 className="Lorem-ipsum">LOREM IPSUM</h1>
+      </div>
+      <div className="right-part">
+        <Heading title={"Welcome to Lorem Ipsum!"} />
+        <div className="details">
+          <Textarea
+            label={"Email/ User Name"}
+            placeholder={"e.g. John Joe"}
+            typevalue={"email"}
+            value={emailOrUsername}
+            onChange={handleChange}
+          />
+          <Textarea
+            label={"password"}
+            placeholder={"Enter Password"}
+            typevalue={"password"}
+            value={password}
+            onChange={handlePassChange}
+          />
         </div>
-    )
-}
+        <div className="forgot-pass" onClick={handleForgotPassword}>
+          <p>Forgot Password?</p>
+        </div>
+        <Button name={"Login"} className={"log"} onChange={handleLogin} />
+        <div className="account">
+          <Account name={"Don't have an account?"} />
+          <Login value={"Register now"} />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default (Home)
+export default Home;
